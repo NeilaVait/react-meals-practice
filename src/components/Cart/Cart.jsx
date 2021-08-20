5779538a82ee50b6bca5cdee535bab1cdd95f3ff
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-20 11:59:49 +0300

    show order btn coditionally

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -15,19 +15,22 @@ const Cart = (props) => {
     </ul>
   );
 
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
   return (
     <Modal onHideCart={props.onHideCart}>
       {cartItems}
 
       <div className={classes.total}>
         <span>Total amount</span>
-        <span>{cartCtx.totalAmount.toFixed(2)}</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button onClick={props.onHideCart} className={classes['button--alt']}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes['button']}>Order</button>}
       </div>
     </Modal>
   );
