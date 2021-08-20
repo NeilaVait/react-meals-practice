b4ee511a8eb7d899d74fca9c99761657829d615d
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-20 10:29:44 +0300

    add form control

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,12 +1,26 @@
 import classes from './MealItemForm.module.css';
 import Input from './../../UI/Input';
+import { useState } from 'react';
 
 const MealItemForm = (props) => {
+  const [formQty, setFormQty] = useState(1);
+
+  const inputValueHandler = (event) => {
+    setFormQty(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formQty);
+  };
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={formSubmitHandler} className={classes.form}>
       <Input
+        onChange={inputValueHandler}
+        value={formQty}
         label="Amount"
-        input={{ id: 'amount_' + props.id, type: 'number', min: 1, max: 5, step: 1, defaultValue: 1 }}
+        input={{ id: 'amount_' + props.id, type: 'number', min: 1, max: 5, step: 1 }}
       />
       <button>+ Add</button>
     </form>
