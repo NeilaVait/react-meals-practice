73faef96d616c2c48d5754eb3a07fc465d0aa14a
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-20 08:49:01 +0300

    CartProvider and cart-context

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 import { useState } from 'react';
+import CartContext from './store/cart-context';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
