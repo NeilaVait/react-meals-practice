e599fd926a336aef74c4e945c5dfda967f6c3030
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-20 11:24:00 +0300

    validation feedback

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -7,6 +7,7 @@ const MealItemForm = (props) => {
   const cartCtx = useContext(CartContext);
 
   const [formQty, setFormQty] = useState('1');
+  const [amountIsValid, setAmountIsValid] = useState(true);
 
   const inputValueHandler = (event) => {
     setFormQty(event.target.value);
@@ -14,8 +15,12 @@ const MealItemForm = (props) => {
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    setAmountIsValid(true);
+
     // isitikinti kad ivesta reiksme tarp 1 ir 5, netuscia ir siusti tik tada
-    if (formQty.trim().length === 0 || +formQty < 1 || +formQty > 5) return;
+    if (formQty.trim().length === 0 || +formQty < 1 || +formQty > 5) return setAmountIsValid(false);
+
     cartCtx.addItem({ id: 'c1', name: 'sushi', price: 12.99, amount: 2 });
   };
 
@@ -27,7 +32,7 @@ const MealItemForm = (props) => {
         label="Amount"
         input={{ id: 'amount_' + props.id, type: 'number', step: 1 }}
       />
-
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
       <button>+ Add</button>
     </form>
   );
