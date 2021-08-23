75fbcf4f9c2befa84572eb4b3e10ebf9ecf43217
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-23 13:19:57 +0300

    add bump class and remove with timeout

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,31 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from './../Cart/CartIcon';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from './../../store/cart-context';
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [btnBumpAdded, setBtnBumpAdded] = useState(false);
+
   // todo count all items quantities
   const numberOfCartItems = cartCtx.items.reduce((total, curObj) => total + curObj.amount, 0);
 
+  const btnClasses = `${classes.button} ${btnBumpAdded ? classes.bump : ''}`;
+
+  const { items } = cartCtx;
+  useEffect(() => {
+    if (items.length === 0) return;
+    setBtnBumpAdded(true);
+    const timerBump = setTimeout(() => {
+      setBtnBumpAdded(false);
+    }, 300);
+    return () => {
+      clearTimeout(timerBump);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
